c4a178aa25fb50d5bfd6e5f6174b718527714e77
Janio Isacura <janioisacura@gmail.com>
2020-07-30 20:11:58 -0300

    Actualización contenido principal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import Users from './components/Users';
+import Products from './components/Products';
+import './scss/App.scss';
 
 function App() {
+  let usuarios = [
+    {name: 'Morgan', mail: 'morgancitoelperro@gmail.com'},
+    {name: 'Milo', mail: 'mililuli@gmail.com'},
+    {name: 'Negro', mail: 'perronegrooficial@gmail.com'},
+  ];
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <Header name="BuB"/>
+        <div className="col-12">
+          <Users />
+        </div>
+      </div>
+      <Products/>
     </div>
+    
+    
   );
 }
 
